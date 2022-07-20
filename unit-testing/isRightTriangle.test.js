aca60ef6e18954669c8e8ceec4be305b405cee35
DESKTOP-UNEKMJK\User <s.buozyte@gmail.com>
2022-07-20 10:06:42 +0300

    Workshope progress

---
+++ unit-testing/isRightTriangle.test.js
@@ -0,0 +1,3 @@
+const isRightTriangle = require('./isRightTriangle');
+
+
--- unit-testing/isTriangle.test.js
+++ unit-testing/isTriangle.test.js
@@ -4,6 +4,22 @@ test('3, 4, 5 is triangle', () => {
     expect(isTriangle(3, 4, 5)).toBe(true);
 });
 
-test('3, 4, 5 is triangle', () => {
-    expect(isTriangle(3, 4, 5)).toBe(false);
+test('0, 4, 5 is not triangle', () => {
+    expect(isTriangle(0, 4, 5)).toBe(false);
+});
+
+test('-3, 4, 7 is not triangle', () => {
+    expect(isTriangle(-3, 4, 7)).toBe(false);
+});
+
+test('A, 4, 5 is not triangle', () => {
+    expect(isTriangle('A', 5, 5)).toBe(false);
+});
+
+test('3000000, 4, 5 is not triangle', () => {
+    expect(isTriangle(3000000, 4, 5)).toBe(false);
+});
+
+test('3.5, 4, 5 is triangle', () => {
+    expect(isTriangle(3.5, 4, 5)).toBe(true);
 });
